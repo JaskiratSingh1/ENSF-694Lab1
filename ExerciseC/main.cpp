1d31ad83f366ab3d07c81e175256af4c0408649d
Jaskirat Singh <38223354+JaskiratSingh1@users.noreply.github.com>
2024-06-26 10:02:21 -0600

    lab1 a complete

---
+++ ExerciseC/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  ExerciseC
+//
+//  Created by Jaskirat Singh on 2024-06-26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
